fed355e2e079f5cfe0bd3ef0752c344d5955de1f
kstasi <nastasiakondaurova@gmail.com>
2020-06-07 16:01:20 +0300

    getAll removed from orders manager

--- client.js
+++ client.js
@@ -3,7 +3,6 @@ const ItemManager = require("./lib/ItemManager");
 const OrderManager = require("./lib/OrderManager");
 const nodeUrl = "/ip4/127.0.0.1/tcp/5001";
 const itemsCid = "bafyreiggcejixnw5wo3gesymhbcwfo7p6yrro2u2se4fcsxikwiexk2efm";
-const ordersCid = "bafyreigxfjbkrvk3ifdhfxbjrswkt2rlqbdt2od55oinhd5jtsocccxpby";
 
 let main = async () => {
     try {
@@ -11,7 +10,7 @@ let main = async () => {
         await ipfs.id();
 
         let itemManager = new ItemManager(ipfs, itemsCid);
-        let orderManager = new OrderManager(ipfs, ordersCid);
+        let orderManager = new OrderManager(ipfs);
         await itemManager.add(
             "seller",
             "name",
@@ -22,14 +21,14 @@ let main = async () => {
             "size",
             "colour",
             [("image", "image")]);
-        await orderManager.add(
+        let cid = await orderManager.add(
             "orderId",
             "dfda83b95889",
             "34e842d6b8fd",
             "zdpuB2oe6QpcftiJvAJkEJy4giHTkcivvURDUz14UdgxnVZc2",
             "zdpuAzZissX6KVNymgHsF5S8jtn79P3tEjTbc9A3eQp9JFAHh"
         );
-        console.log(await orderManager.getAll());
+        console.log(await orderManager.getByCid(cid));
         console.log(await itemManager.getAll());
     } catch (e) {
         console.log(e);

b93eb511a0332f3bd03635174df4ce4f361af074
kstasi <nastasiakondaurova@gmail.com>
2020-06-08 21:04:32 +0300

    updated lib

--- client.js
+++ client.js
@@ -2,18 +2,17 @@ const ipfsClient = require("ipfs-http-client");
 const ItemManager = require("./lib/ItemManager");
 const OrderManager = require("./lib/OrderManager");
 const nodeUrl = "/ip4/127.0.0.1/tcp/5001";
-const crypto = require('crypto');
+const cryptico = require('cryptico');
 
 let main = async () => {
     try {
+
+        var privateKey = cryptico.generateRSAKey("", 2048);
+        var publicKey = cryptico.publicKeyString(privateKey);
+
         let ipfs = ipfsClient(nodeUrl);
         await ipfs.id();
         let marketContractAddress = "KT1N5h4c7kZ85DDjrYGqv6xmQRwufG2x2c5c";
-        const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
-            modulusLength: 2048,
-            publicKeyEncoding: { type: 'spki', format: 'pem' },
-            privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
-        })
         let itemManager = await ItemManager.createInstance(ipfs, marketContractAddress);
         let orderManager = new OrderManager(ipfs);
         await itemManager.add(
